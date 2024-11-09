7a60edae47c451e57654bc8c2233237b8bcd165c
Khushant Wankhede <yashwankhede53@gmail.com>
2024-11-08 23:17:10 -0800

    apply html css using javascript

---
+++ Javascript/third.js
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <title>
+            The javascript example
+        </title>
+    </head>
+    <body style="background-color: lightblue;font-weight: 50px;">
+        <!-- NOTE : SCRIPT niche likho taki sab upload ho jaye  -->
+        <div id="output">ASDF</div>
+        
+        <script lang="javascript" type="text/javascript" >
+           document.getElementById("output").innerHTML = "HELLO KHUSHANT";
+            document.getElementById("output").style.color = "blue";
+            document.getElementById("output").style.fontSize = "25px";
+            document.getElementById("output").style.fontFamily = "Monospace";
+           
+        </script>
+
+    </body>
+</html>
