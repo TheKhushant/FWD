a6c4a3e8335f7ae1c1cc3542d28ed3f3e912be51
Khushant Wankhede <yashwankhede53@gmail.com>
2024-11-08 23:07:17 -0800

    use of console
    inspect and then consoloe

---
+++ Javascript/second.js
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>
+            The javascript example
+        </title>
+    </head>
+    <body style="background-color: lightblue;font-weight: 50px;">
+        <script lang="javascript" type="text/javascript" >
+           console.log("HELLO WORLD");
+        </script>
+        please open the console to see hello world
+    </body>
+</html>
