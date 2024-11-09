fec0010eb5bdb603cf072f02d30c7f8f98ab2d93
Khushant Wankhede <yashwankhede53@gmail.com>
2024-11-08 23:03:32 -0800

    Create FIRST.js

---
+++ Javascript/FIRST.js
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <title>
+            The javascript example
+        </title>
+    </head>
+    <body style="background-color: lightblue;font-weight: 50px;">
+        
+        <script lang="javascript" type="text/javascript" >
+            document.write("Hello World!")
+            alert("This is alert !");
+            confirm("Please confirm your name is KHUSHANT");
+            prompt("this is a prompt !");
+            
+            document.write("We implemented JS dialogue boxes");
+        </script>
+    </body>
+</html>
